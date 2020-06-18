8d3434c3c6fcfc9653d3b90c274d89b5874b95ae
Husain Adam Askari <haskari@ucsc.edu>
2020-06-18 11:43:37 -0700

    issues

--- MovieApp/src/app/movies/movie-details/movie-details.component.ts
+++ MovieApp/src/app/movies/movie-details/movie-details.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
 
 @Component({
-  selector: 'app-movie-details',
+  selector: 'movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.css']
 })
-export class MovieDetailsComponent implements OnInit {
 
-  constructor() { }
+export class MovieDetailsComponent {
+  @Input()
+  movie: Movie;
 
-  ngOnInit(): void {
+  @Input()
+  createHandler: Function;
+  @Input()
+  updateHandler: Function;
+  @Input()
+  deleteHandler: Function;
+
+  constructor (private movieService: MovieService) {}
+
+  createMovie(movie: Movie) {
+    this.movieService.createMovie(movie).then((newMovie: Movie) => {
+      this.createHandler(newMovie);
+    });
   }
 
+  updateMovie(movie: Movie): void {
+    this.movieService.updateMovie(movie).then((updatedMovie: Movie) => {
+      this.updateHandler(updatedMovie);
+    });
+  }
+
+  deleteMovie(movieId: String): void {
+    this.movieService.deleteMovie(movieId).then((deletedMovieId: String) => {
+      this.deleteHandler(deletedMovieId);
+    });
+  }
 }
